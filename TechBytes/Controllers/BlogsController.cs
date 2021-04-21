e0176d68ccc54fbc678ede4de7b1497d495b2fce
Aodai <odai@odaiq.co>
2021-04-21 11:25:37 +0300

    Created controllers and views for Blog and Post entities.

---
+++ TechBytes/Controllers/BlogsController.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using TechBytes.ApplicationLogic.Models;
+using TechBytes.ApplicationLogic.Services;
+using TechBytes.DataAccess;
+
+namespace TechBytes.Controllers
+{
+    public class BlogsController : Controller
+    {
+        private readonly BlogsService blogsService;
+
+        public BlogsController(BlogsService blogsService)
+        {
+            this.blogsService = blogsService;
+        }
+
+        // GET: Blogs
+        public IActionResult Index()
+        {
+            var blogs = blogsService.GetAll();
+            return View(blogs);
+        }
+
+        // GET: Blogs/Details/5
+        public IActionResult Details(Guid? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var blog = blogsService.GetBlogById(id.ToString());
+
+            if (blog == null)
+            {
+                return NotFound();
+            }
+
+            return View(blog);
+        }
+
+        // GET: Blogs/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Blogs/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create([Bind("ID,Url")] Blog blog)
+        {
+            if (ModelState.IsValid)
+            {
+                blog.ID = Guid.NewGuid();
+                blogsService.Add(blog);
+                return RedirectToAction(nameof(Index));
+            }
+            return View(blog);
+        }
+
+        // GET: Blogs/Edit/5
+        public IActionResult Edit(Guid? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var blog = blogsService.GetBlogById(id.ToString());
+
+            if (blog == null)
+            {
+                return NotFound();
+            }
+            return View(blog);
+        }
+
+        // POST: Blogs/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(Guid id, [Bind("ID,Url")] Blog blog)
+        {
+            if (id != blog.ID)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    blogsService.Update(blog);
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!BlogExists(blog.ID.ToString()))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(blog);
+        }
+
+        // GET: Blogs/Delete/5
+        public IActionResult Delete(Guid? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var blog = blogsService.GetBlogById(id.ToString());
+
+            if (blog == null)
+            {
+                return NotFound();
+            }
+
+            return View(blog);
+        }
+
+        // POST: Blogs/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public IActionResult DeleteConfirmed(string id)
+        {
+            var blog = blogsService.GetBlogById(id);
+            blogsService.Delete(blog);
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool BlogExists(string id)
+        {
+            return blogsService.GetBlogById(id) != null;
+        }
+    }
+}
---
+++ TechBytes/Controllers/PostsController.cs
@@ -0,0 +1,177 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using TechBytes.ApplicationLogic.Models;
+using TechBytes.ApplicationLogic.Services;
+using TechBytes.DataAccess;
+
+namespace TechBytes.Controllers
+{
+    public class PostsController : Controller
+    {
+        private readonly PostsService postsService;
+        private readonly BlogsService blogsService;
+
+        public PostsController(PostsService postsService, BlogsService blogsService)
+        {
+            this.postsService = postsService;
+            this.blogsService = blogsService;
+        }
+
+        // GET: Posts
+        public IActionResult Index()
+        {
+            return View(postsService.GetAll());
+        }
+
+        // GET: Posts/Details/5
+        public IActionResult Details(Guid? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+            
+            var post = postsService.GetPostById(id.ToString());
+
+            //var post = await _context.Posts
+            //    .Include(p => p.Blog)
+            //    .FirstOrDefaultAsync(m => m == id);
+
+            if (post == null)
+            {
+                return NotFound();
+            }
+
+            return View(post);
+        }
+
+        // GET: Posts/Create
+        public IActionResult Create()
+        {
+            ViewData["BlogID"] = blogsService.GetAll()
+                .Select(c => new SelectListItem() { Text = c.Url, Value = c.ID.ToString() })
+                .ToList();
+        //    ViewData["BlogID"] = new SelectList(_context.Blogs, "ID", "ID");
+            return View();
+        }
+
+        // POST: Posts/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create([Bind("ID,BlogID,Published,Modified,Title,Content")] Post post)
+        {
+            if (ModelState.IsValid)
+            {
+                post.ID = Guid.NewGuid();
+                postsService.Add(post);
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["BlogID"] = blogsService.GetAll()
+                .Select(c => new SelectListItem() { Text = c.Url, Value = c.ID.ToString() })
+                .ToList();
+            //ViewData["BlogID"] = new SelectList(_context.Blogs, "ID", "ID", post.BlogID);
+            return View(post);
+        }
+
+        // GET: Posts/Edit/5
+        public IActionResult Edit(Guid? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var post = postsService.GetPostById(id.ToString());
+            if (post == null)
+            {
+                return NotFound();
+            }
+            ViewData["BlogID"] = blogsService.GetAll()
+                .Select(c => new SelectListItem() { Text = c.Url, Value = c.ID.ToString() })
+                .ToList();
+            //ViewData["BlogID"] = new SelectList(_context.Blogs, "ID", "ID", post.BlogID);
+            return View(post);
+        }
+
+        // POST: Posts/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(Guid id, [Bind("ID,BlogID,Published,Modified,Title,Content")] Post post)
+        {
+            if (id != post.ID)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    postsService.Update(post);
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!PostExists(post.ID.ToString()))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["BlogID"] = blogsService.GetAll()
+                .Select(c => new SelectListItem() { Text = c.Url, Value = c.ID.ToString() })
+                .ToList();
+            //ViewData["BlogID"] = new SelectList(_context.Blogs, "ID", "ID", post.BlogID);
+            return View(post);
+        }
+
+        // GET: Posts/Delete/5
+        public IActionResult Delete(Guid? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var post = postsService.GetPostById(id.ToString());
+
+            //var post = await _context.Posts
+            //    .Include(p => p.Blog)
+            //    .FirstOrDefaultAsync(m => m.ID == id);
+            if (post == null)
+            {
+                return NotFound();
+            }
+
+            return View(post);
+        }
+
+        // POST: Posts/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public IActionResult DeleteConfirmed(string id)
+        {
+            var post = postsService.GetPostById(id);
+            postsService.Delete(post);
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool PostExists(string id)
+        {
+            return postsService.GetPostById(id) != null;
+        }
+    }
+}
